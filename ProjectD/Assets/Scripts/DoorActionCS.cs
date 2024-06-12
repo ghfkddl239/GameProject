3dc4309d8ef9efedf165fe6fe67eb05cd9e02a82
tdays31 <ghfkddl239@gmail.com>
2024-06-12 18:14:17 +0900

    door2
    문열고닫기 상호작용

--- ProjectD/Assets/Scripts/DoorActionCS.cs
+++ ProjectD/Assets/Scripts/DoorActionCS.cs
@@ -6,35 +6,120 @@ public class DoorActionCS : MonoBehaviour, IAction
 {
     float deg; //각도
     float nowDeg;
+    float vector;
+    bool isShow = false;
+    bool isOpen = false;
+    float showTimer = 1;
 
     public float objSpeed; //원운동 속도
     public Transform rotatePos;
+    public Material outlineHighlight;
 
     // Start is called before the first frame update
     void Start()
     {
-        print(transform.localEulerAngles.y);
         deg = 90.0f;
-        nowDeg = 0;
+        nowDeg = 0f;
+        vector = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Mathf.Abs(transform.localEulerAngles.y) < deg)
+    }
+
+    public void Action(Transform playerTr)
+    {
+        GetAngle(playerTr);
+        StartCoroutine(DoorOpenAndClose());
+    }
+
+    private IEnumerator DoorOpenAndClose()
+    {
+        yield return new WaitForFixedUpdate();
+        print(isOpen);
+        if (isOpen)
         {
-            nowDeg += objSpeed * Time.deltaTime;
-            DoorOpen(nowDeg);
+            if (transform.localEulerAngles.y < 0)
+            {
+                nowDeg += objSpeed * Time.deltaTime;
+            }
+            else
+            {
+                nowDeg -= objSpeed * Time.deltaTime;
+            }
+            print("AAA / " + nowDeg);
+            transform.RotateAround(rotatePos.position, Vector3.up, nowDeg);
+
+            if (Mathf.Abs(transform.localEulerAngles.y) < 0.1f)
+            {
+                nowDeg = 0;
+                isOpen = false;
+            }
+            else
+            {
+                StartCoroutine(DoorOpenAndClose());
+            }
+        }
+        else
+        {
+            nowDeg += objSpeed * Time.deltaTime * vector;
+            print("BBB / " + nowDeg);
+            transform.RotateAround(rotatePos.position, Vector3.up, nowDeg);
+            if (Mathf.Abs(transform.localEulerAngles.y) > 89.9f)
+            {
+                nowDeg = 0f;
+                isOpen = true;
+            }
+            else
+            {
+                StartCoroutine(DoorOpenAndClose());
+            }
+            print(Mathf.Abs(transform.localEulerAngles.y));
         }
     }
 
-    public void Action()
+    public void ShowOutline()
     {
-        deg = deg == 90.0f ? 0 : 90.0f;
+        showTimer = 1;
+        if (isShow) return;
+        if (GetComponent<MeshRenderer>() is not null)
+        {
+            isShow = true;
+            GetComponent<MeshRenderer>().materials = new Material[2] { GetComponent<MeshRenderer>().material, outlineHighlight };
+            StartCoroutine(ShowCheck());
+        }
     }
 
-    private void DoorOpen(float deg)
+    private IEnumerator ShowCheck()
     {
-        transform.RotateAround(rotatePos.position, Vector3.up, deg);
+        showTimer -= 0.5f;
+        if (showTimer <= 0)
+        {
+            isShow = false;
+            GetComponent<MeshRenderer>().materials = new Material[2] { GetComponent<MeshRenderer>().material, null };
+        }
+        yield return new WaitForSeconds(0.5f);
+        if (isShow)
+        {
+            StartCoroutine(ShowCheck());
+        }        
+    }
+
+    public void GetAngle(Transform playerTr)
+    {
+        Vector3 v1 = rotatePos.forward;
+        Vector3 v2 = rotatePos.position - playerTr.position;
+
+        Vector3 v = v2 - v1;
+        float angle = Mathf.Atan2(v.z, v.x) * Mathf.Rad2Deg;
+        if (angle < 0)
+        {
+            vector = -1f;
+        }
+        else if (angle >= 0)
+        {
+            vector = 1f;
+        }
     }
 }
